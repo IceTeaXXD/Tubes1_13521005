847d38c6faf38bdd1028b7f4aaebdc5d3e298182
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-10-18 22:23:36 +0700

    fix: sideways bot

--- src/HCSidewaysBot.java
+++ src/HCSidewaysBot.java
@@ -9,7 +9,7 @@ public class HCSidewaysBot extends Bot {
 
     public int[] move(Button[][] board, int roundsLeft) {
         int[] selection = new int[2];
-        int eval = 0;
+        int eval = Integer.MIN_VALUE;
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if (board[i][j].getText().equals("")) {
@@ -26,17 +26,15 @@ public class HCSidewaysBot extends Bot {
                     if (j != 7 && board[i][j + 1].getText().equals(enemySymbol)) {
                         res++;
                     }
-                    if (res > eval) {
-                        eval = res;
-                        selection[0] = i;
-                        selection[1] = j;
-                    } else if (res < eval) {
+                    if (res < eval) {
                         return selection;
                     }
+                    eval = res;
+                    selection[0] = i;
+                    selection[1] = j;
                 }
             }
         }
         return selection;
-
     }
 }
